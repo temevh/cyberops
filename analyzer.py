d64e431dc0f7295c4d7268c102037e19b8f3ecd5
temevh <temehama@gmail.com>
2025-03-24 17:26:24 +0900

    showing TCP and udp packets

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,22 @@
 import pyshark
-cap = pyshark.FileCapture('capture.pcapng')
-print(cap[0])
+
+def network_conversation(packet):
+  try:
+    protocol = packet.transport_layer
+    source_address = packet.ip.src
+    source_port = packet[packet.transport_layer].srcport
+    destination_address = packet.ip.dst
+    destination_port = packet[packet.transport_layer].dstport
+    return (f'{protocol} {source_address}:{source_port} --> {destination_address}:{destination_port}')
+  except AttributeError as e:
+    pass
+
+capture = pyshark.FileCapture('capture.pcapng')
+conversations = []
+for packet in capture:
+  results = network_conversation(packet)
+  if results != None:
+    conversations.append(results)
+
+for item in sorted(conversations):
+  print (item)
